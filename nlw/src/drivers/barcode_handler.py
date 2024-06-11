f1cbb75aa5613663aba1e4484788914a5967bcc5
Cyro Liduario <cyroliduario@hotmail.com>
2024-06-11 10:20:53 -0300

    Adding folder to save barcodes

--- nlw/src/drivers/barcode_handler.py
+++ nlw/src/drivers/barcode_handler.py
@@ -4,7 +4,7 @@ from barcode.writer import ImageWriter
 class BarcodeHandler:
     def create_barcode(self, product_code: str) -> str:
         tag = Code128(product_code, writer=ImageWriter())
-        path_from_tag = f'{tag}'
+        path_from_tag = f'barcodes/{tag}'
         tag.save(path_from_tag)
 
         return path_from_tag
